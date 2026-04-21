f6c7d5d4bc2afe3058b8bd5b23f7063790c90727
Mritunjay Sahu <omsahu1032@gmail.com>
2026-04-21 17:28:28 +0530

    updated Maindashboard

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Rabbit
 
 using static data from the file {rabbit_200_posts_data.json}
+created Login and Sign up page
+structured maindashboard,Navbar , SidePanel , MainPanel
--- src/components/MainDashBoard.jsx
+++ src/components/MainDashBoard.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import { useState } from 'react';
 import FetchingData from './FetchingData';
-import Login from '../Pages/Login';
+import Navbar from './Navbar';
+import Main from './Main';
+import SidePanel from './SidePanel';
+
 
 const MainDashBoard = () => {
     const [posts , setPosts] = useState("");
     return(
         <div>
             <FetchingData setPosts={setPosts}/>
-            <h1>Welcome to Rabbit</h1>
+            <div style={{borderBottom:"1px solid Black", marginBottom:"1px", paddingBottom:"20px"}}>
+                <Navbar/>
+            </div>
+            <div style={{display:"flex"}}>
+                <div style={{borderRight:"1px solid black", height:"100vh",marginRight:"40px",paddingRight:"100px", paddingTop:"50px"}} >
+                    <SidePanel/>
+                </div>
+                <div style={{padding:"100px"}}>
+                    <Main/>
+                </div>
+            </div>
         </div>
     )
 }
