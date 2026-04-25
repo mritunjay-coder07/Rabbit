927cc4c9a134a79cae6c6c0e28211e3f2b300b06
Muskaan <muskaanramrakhyani@gmail.com>
2026-04-25 23:23:15 +0530

    Final Navbar updation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import "../Styles/Navbar.css"
+import rabbit from "../Pages/rabbit.png";
+import logo from "../Pages/logo.jpeg";  
 
 const Navbar = () => {
   return (
     <div style={{display:"flex"}} className='navbar'>
         <div className='logo'>
-          <img src="src/Pages/rabbit.png" alt="" style={{width:140,height:80,borderRadius:50, marginTop:"15px"}} />
+          <img src={rabbit} alt="" style={{width:140,height:80,borderRadius:50, marginTop:"15px"}} />
         </div>
         <div style={{display:"flex", position:"relative"}}>
           <div className='search-logo'>
-            <img src="src/Pages/logo.jpeg" alt="" style={{width:35,height:35,borderRadius:50, position:"relative",left:"0px", marginTop:"4px"}}/>
+            <img src={logo} alt="" style={{width:35,height:35,borderRadius:50, position:"relative",left:"0px", marginTop:"4px"}}/>
           </div>
           <input placeholder='        Search' className='search' style={{width: "700px",
     padding: "12px 20px",
